262f06eb7faf893a12e7f486743b68542f05dee1
AgustinBadi <absalinas@uc.cl>
2022-08-02 12:32:13 -0400

    Actualizar funciones matemáticas

--- funciones_numéricas.hs
+++ funciones_numéricas.hs
@@ -2,17 +2,17 @@
 -- Secuencia de números primos
 -- primos = 1 : [ a | a <- [1,2..], b <- [1,2..(a-1)], mod a b == 0] 
 
--- Secuencia de números de fibonacci
-
+-- Secuencia infinita de números de fibonacci
 fib :: Integral t => t -> t -> [t]
-infinteFibonacci :: [Integer]
+infinitosFibonacci :: [Integer]
 
 fib a b = let suma = a + b in suma:(fib b suma)   
-infinteFibonacci = 1:(fib 0 1)
+infinitosFibonacci = 1:(fib 0 1)
 
--- Implementación mod
 
+-- Implementación función mod
 mod' :: Int -> Int -> Int
+
 mod' a b
  | diferencia == 0 = 0
  | diferencia == b = 0
@@ -20,15 +20,15 @@ mod' a b
  | diferencia > b = mod' diferencia b
  where diferencia = a - b 
 
- -- Suma los números impares dentro de una lista
 
+ -- Suma los números impares dentro de una lista
 sumarImpares [] = 0
 sumarImpares (a:arr)
  | a `mod` 2 == 0 = sumarImpares arr
  | a `mod` 2 == 1 = a + (sumarImpares arr )
 
--- Devuelve los valores absolutos de una lista
 
+-- Devuelve los valores absolutos de una lista
 abs' [] = []
 abs' (a:arr)
  | a < 0 = a * (-1) : ( abs' arr)
